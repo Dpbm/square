9736d75abf3a4508a767ae3f3d22bdc9d0330627
Dpbm <dpbm136@gmail.com>
2022-08-27 17:24:47 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const rows = 3;
+const columns = 3;
+
+const getMiddle = (totalCharacters, character) => (Array(totalCharacters)
+    .fill(null)
+    .map(() => character)).join("")
+
+
+function square(rows, columns) {
+    if (rows <= 0 || columns <= 0) throw new Error('Invalid Input!');
+
+    const totalMiddleCharacters = columns - 2 < 0 ?
+        0 :
+        columns - 2;
+
+    const firstAndLastRow = `+${getMiddle(totalMiddleCharacters, '-')}${columns == 1 ? '' : '+'}`;
+
+    console.log(firstAndLastRow);
+
+    if (rows === 1) return;
+
+    for (let actualRow = 0; actualRow < rows - 2; actualRow++) console.log(`|${getMiddle(totalMiddleCharacters, ' ')}${columns == 1 ? '' : '|'}`)
+
+    console.log(firstAndLastRow);
+
+}
+
+
+
+square(rows, columns);
