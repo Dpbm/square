91fdcda2c3d4600f78e3fd54df4ccf0187a17165
Dpbm <dpbm136@gmail.com>
2022-08-28 07:59:06 -0300

    changed middle rows text to a constant

--- main.js
+++ main.js
@@ -2,26 +2,27 @@ const rows = 3; // change this value to increase or decrease the number of rows
 const columns = 3; // change this value to increase or decrease the number of columns 
 
 const getMiddle = (totalCharacters, character) => (Array(totalCharacters)
-    .fill(null)
-    .map(() => character)).join("")
+  .fill(null)
+  .map(() => character)).join("")
 
 
 function square(rows, columns) {
-    if (rows <= 0 || columns <= 0) throw new Error('Invalid Input!');
+  if (rows <= 0 || columns <= 0) throw new Error('Invalid Input!');
 
-    const totalMiddleCharacters = columns - 2 < 0 ?
-        0 :
-        columns - 2;
+  const totalMiddleCharacters = columns - 2 < 0 ?
+    0 :
+    columns - 2;
 
-    const firstAndLastRow = `+${getMiddle(totalMiddleCharacters, '-')}${columns == 1 ? '' : '+'}`;
+  const firstAndLastRow = `+${getMiddle(totalMiddleCharacters, '-')}${columns == 1 ? '' : '+'}`;
+  const middleRows = `|${getMiddle(totalMiddleCharacters, ' ')}${columns == 1 ? '' : '|'}`;
 
-    console.log(firstAndLastRow);
+  console.log(firstAndLastRow);
 
-    if (rows === 1) return;
+  if (rows === 1) return;
 
-    for (let actualRow = 0; actualRow < rows - 2; actualRow++) console.log(`|${getMiddle(totalMiddleCharacters, ' ')}${columns == 1 ? '' : '|'}`)
+  for (let actualRow = 0; actualRow < rows - 2; actualRow++) console.log(middleRows);
 
-    console.log(firstAndLastRow);
+  console.log(firstAndLastRow);
 
 }
 
